3c5b90104e57ad6da78ba20551643b4510fa2834
Елена Голованова <43187051+Lena381706-1@users.noreply.github.com>
2019-10-20 22:36:21 +0300

    First

---
+++ modules/task_1/golovanova_e_min_elem_matrix/main.cpp
@@ -0,0 +1,42 @@
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "../../../modules/task_1/golovanova_e_min_elem_matrix/min_elem_matrix.h"
+
+TEST(min_elem_matrix, test1) {
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0) {
+		ASSERT_ANY_THROW(getRandomMatrix(-10, 10));
+	}
+}
+
+TEST(min_elem_matrix, test2) {
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	std::vector<int> M(25);
+	for (int i = 1; i < 26; i++)
+		M[i] = i;
+	int M_min = 1;
+	int rez = MinElemMatrix(M, 5, 5);
+	if (rank == 0) {
+		EXPECT_EQ(rez, M_min);
+	}
+}
+
+
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	MPI_Init(&argc, &argv);
+
+	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+	::testing::TestEventListeners& listeners =
+		::testing::UnitTest::GetInstance()->listeners();
+
+	listeners.Release(listeners.default_result_printer());
+	listeners.Release(listeners.default_xml_generator());
+
+	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+	return RUN_ALL_TESTS();
+}
