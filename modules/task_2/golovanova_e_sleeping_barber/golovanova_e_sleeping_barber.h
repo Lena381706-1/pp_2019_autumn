790574c862b8231b0cdbdb0bd03a713abcd13ad7
Елена Голованова <43187051+Lena381706-1@users.noreply.github.com>
2019-11-20 16:35:56 +0300

    1

---
+++ modules/task_2/golovanova_e_sleeping_barber/golovanova_e_sleeping_barber.h
@@ -0,0 +1,9 @@
+// Copyright 2019 Golovanova Elena
+#ifndef MODULES_TASK_2_GOLOVANOVA_E_SLEEPING_BARBER_SLEEPING_BARBER_H_
+#define MODULES_TASK_2_GOLOVANOVA_E_SLEEPING_BARBER_SLEEPING_BARBER_H_
+
+void Barber(int visitorTime, int size);
+void Visitor(int visitorTime, int rank);
+void SleepingBarber();
+
+#endif  // MODULES_TASK_2_GOLOVANOVA_E_SLEEPING_BARBER_H_
